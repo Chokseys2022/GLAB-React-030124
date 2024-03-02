140e92b6de72365e1cdae7f93f48d3ae89e03c1b
S C <choksey2022@gmail.com>
2024-03-02 09:50:14 -0800

    spelling error fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ import imageSrc from './download.jpg';
 function Header () {
     return (
         <div class ="headerContainer">
-        <h1> Qutoes for the day </h1>
+        <h1> Quotes for the day </h1>
         <img src={imageSrc} alt="Description of the image" className="centeredImg wBorderandShadow" />
         </div>
     )
