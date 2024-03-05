046bca6cbd8ea7348ced44bc48467ed7efd1fcce
Chokseys2022 <choksey2022@gmail.com>
2024-03-04 17:06:20 -0800

    deleted commented code

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,3 @@
-// function Header() {
-//   return (<h1>Simple React Application</h1>);
-// }
-
-// function Content(props) {
-//   return (<p style={{color: props.color}}>{props.text}</p>);
-// }
-
-// function Footer() {
-//   return (<h1>Created by Me, of course.</h1>);
-// }
-
-// function App() {
-//   return (
-//     <>
-//       <Header />
-//       <Content color="blue" text="This is my first React Application!" />
-//       <Content color="red" text="Wish me luck..." />
-//       <Content color="green" text="I think I've got it!" />
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
-
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Content from "../components/Content";
